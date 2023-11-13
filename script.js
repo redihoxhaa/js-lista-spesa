d5f292ef8fd0c9dd42d671f0acb09eeafc0dabb2
redihoxhaa <redi.hoxha1996@gmail.com>
2023-11-13 14:56:58 +0100

    feat: stilizzazione 2.0

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 "use strict";
 
-const shoppingList = ["Pane", "Acqua", "Cipolle", "Ketchup", "Pasta", "Ricotta", "Grana", "Melanzane", "Latte", "Brioschi"];
+const shoppingList = ["Pane", "Acqua", "Cipolle", "Ketchup", "Pasta", "Ricotta", "Biscotti", "Melanzane", "Latte", "Brioschi"];
 const divShoppingList = document.querySelector(".list");
 let contatore = 0;
 
 while (contatore < shoppingList.length) {
     const product = document.createElement("li");
     product.classList.add("product");
-    product.innerHTML = `<input class="form - check - input me - 1" type="checkbox" value="" id="firstCheckboxStretched"><label class="form-check-label" for="firstRadio">${shoppingList[contatore]}</label>`;
+    product.innerHTML = `<input class="form-check-input" type="checkbox" value="" id="radio${contatore}"><label class="form-check-label" for="radio${contatore}">${shoppingList[contatore]}</label>`;
     divShoppingList.append(product);
     contatore++;
 }
