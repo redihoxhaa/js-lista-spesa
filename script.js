cba7a742f498c37fb8107d3d2ae32ab8f52aa44b
redihoxhaa <redi.hoxha1996@gmail.com>
2023-11-13 14:04:47 +0100

    feat: abbellimento CSS

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let contatore = 0;
 while (contatore < shoppingList.length) {
     const product = document.createElement("li");
     product.classList.add("product");
-    product.append(shoppingList[contatore]);
+    product.innerHTML = `<input class="form - check - input me - 1" type="checkbox" value="" id="firstCheckboxStretched"><label class="form-check-label" for="firstRadio">${shoppingList[contatore]}</label>`;
     divShoppingList.append(product);
     contatore++;
 }
